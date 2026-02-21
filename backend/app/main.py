6cd404a4bea9e9b84a4ab9188ad23b10f74c5ab8
Mohithaa7 <mohithaa.csbs>
2026-02-22 00:20:06 +0530

    Add registration endpoint and security module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,46 @@
-from fastapi import FastAPI
-from .database import engine, Base
-from . import models  # This is IMPORTANT to register models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .database import engine, Base, SessionLocal
+from .security import hash_password
+from . import models
 
 app = FastAPI()
 
-# Create tables in database
 Base.metadata.create_all(bind=engine)
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def root():
     return {"message": "SecureKYC Backend Running Successfully"}
+
+
+@app.post("/register")
+def register_user(email: str, password: str, db: Session = Depends(get_db)):
+
+    # Check if user already exists
+    existing_user = db.query(models.User).filter(models.User.email == email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    # Hash password
+    hashed_pwd = hash_password(password)
+
+    # Create user
+    new_user = models.User(
+        email=email,
+        hashed_password=hashed_pwd
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
